3ccf0fe0b7a2b7fd6250945d70506f564fb986b6
belong112 <b05901159@ntu.edu.tw>
2021-01-11 17:41:44 +0800

    add total profit

--- components/MyTable.js
+++ components/MyTable.js
@@ -12,17 +12,40 @@ import {
 import styles from "./mytable.module.css";
 
 // self-defined-components
+const fakeData = [
+  { firm: "India", investRatio: 0.13213, expectedProfit: 3287263 },
+  { firm: "China", investRatio: 0.0235, expectedProfit: 9596961 },
+  { firm: "Italy", investRatio: 0.0334, expectedProfit: 301340 },
+  { firm: "United States", investRatio: 0.05, expectedProfit: 9833520 },
+  { firm: "Canada", investRatio: 0.103, expectedProfit: 9984670 },
+  { firm: "Australia", investRatio: 0.0754, expectedProfit: 7692024 },
+  { firm: "Germany", investRatio: 0.0892, expectedProfit: 357578 },
+  { firm: "Ireland", investRatio: 0.034857, expectedProfit: 70273 },
+  { firm: "Mexico", investRatio: 0.0177691, expectedProfit: 1972550 },
+  { firm: "Japan", investRatio: 0.11, expectedProfit: 377973 },
+  { firm: "France", investRatio: 0.0123, expectedProfit: 640679 },
+  { firm: "United Kingdom", investRatio: 0.067545757, expectedProfit: 242495 },
+  { firm: "Russia", investRatio: 0.0146793744, expectedProfit: 17098246 },
+  { firm: "Nigeria", investRatio: 0.0200962417, expectedProfit: 923768 },
+  { firm: "Brazil", investRatio: 0.0210147125, expectedProfit: 8515767 },
+  { firm: "Taiwan", investRatio: 0.099022, expectedProfit: 990679 },
+  { firm: "Korea", investRatio: 0.06748057, expectedProfit: 241495 },
+  { firm: "Thailand", investRatio: 0.01793744, expectedProfit: 278246 },
+  { firm: "Denmark", investRatio: 0.0962417, expectedProfit: 912768 },
+  { firm: "Chili", investRatio: 0.01234563, expectedProfit: 124322 },
+];
+
 const columns = [
   { id: "firm", label: "公司名稱", minWidth: 170 },
   {
-    id: "invest",
+    id: "investRatio",
     label: "投資比例",
     minWidth: 170,
     align: "right",
     format: (value) => value.toLocaleString("en-US"),
   },
   {
-    id: "expected",
+    id: "expectedProfit",
     label: "預期報酬",
     minWidth: 170,
     align: "right",
@@ -30,36 +53,25 @@ const columns = [
   },
 ];
 
-function createData(firm, invest, expected) {
-  return { firm, invest, expected };
+function createData(firm, investRatio, expectedProfit) {
+  investRatio = `${(investRatio * 100).toFixed(2)} %`;
+  return { firm, investRatio, expectedProfit };
 }
 
-const rows = [
-  createData("India", 1324171354, 3287263),
-  createData("China", 1403500365, 9596961),
-  createData("Italy", 60483973, 301340),
-  createData("United States", 327167434, 9833520),
-  createData("Canada", 37602103, 9984670),
-  createData("Australia", 25475400, 7692024),
-  createData("Germany", 83019200, 357578),
-  createData("Ireland", 4857000, 70273),
-  createData("Mexico", 126577691, 1972550),
-  createData("Japan", 126317000, 377973),
-  createData("France", 67022000, 640679),
-  createData("United Kingdom", 67545757, 242495),
-  createData("Russia", 146793744, 17098246),
-  createData("Nigeria", 200962417, 923768),
-  createData("Brazil", 210147125, 8515767),
-  createData("Taiwan", 99022000, 990679),
-  createData("Korea", 6748057, 241495),
-  createData("Tailand", 1793744, 278246),
-  createData("Denmark", 2962417, 912768),
-  createData("Chili", 1234563, 124322),
-];
+const calculateTotalProfit = (investData) => {
+  let totalProfit = 0;
+  investData.forEach(({ investRatio, expectedProfit }) => {
+    totalProfit += investRatio * expectedProfit;
+  });
+  return totalProfit.toFixed(0);
+};
 
-const MyTable = ({}) => {
+const MyTable = ({ investData = fakeData }) => {
   const [page, setPage] = useState(0);
   const rowsPerPage = 10;
+  const rows = investData.map(({ firm, investRatio, expectedProfit }) => {
+    return createData(firm, investRatio, expectedProfit);
+  });
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -101,6 +113,13 @@ const MyTable = ({}) => {
                   </TableRow>
                 );
               })}
+            <TableRow>
+              <TableCell rowSpan={2} />
+              <TableCell>預期總報酬</TableCell>
+              <TableCell align="right">
+                {calculateTotalProfit(investData)}
+              </TableCell>
+            </TableRow>
           </TableBody>
         </Table>
       </TableContainer>
